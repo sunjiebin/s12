c8782d9518171e154ae094a44348f838e4457415
孙杰彬 <sunjb@my089.net>
2018-07-18 20:43:50 +0800

    set集合示例

--- week4/16json与pickle.py
+++ week4/16json与pickle.py
@@ -24,6 +24,22 @@ with open('dic','w+') as f:
 with open('dic','r') as r:
     data=json.loads(r.read()) #注意是loads,不是load
     print(data)
+'''用dump/load的另一种写法，
+和上面的效果是一样的，只是写法不一样，
+下面的写法更常用，更简洁'''
+with open('dic','w') as f:
+    json.dump(a_dic,f)
+'''注意python3这里dump不能是多次，如果多次dump之后，load就会报错,
+在python2里面，是可以dump多次的，同时load的时候也要load多次
+才行，且要按顺序来load，这样做其实没什么用。所以建议在dump时，
+一个文件只dump一次，然后load，再用的时候重新dump。如果要dump
+多次，建议每个dump都单独一个文件，就像虚拟机的快照一样，一个
+快照一个文件，相互独立'''
+ #   json.dump('helo',f)
+with open('dic','r') as r:
+    print('jsonload的值为：')
+    print(json.load(r))
+
 
 import pickle
 '''pickle是Python专有的序列化模块，可以序列化各种数据，
