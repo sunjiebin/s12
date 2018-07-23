d99c13061b475089e3b97ccb4ab02e4b0286e45c
sunjiebin <33552080+sunjiebin@users.noreply.github.com>
2018-07-23 23:25:01 +0800

    购物车商家版

--- week2/homework-商家版.py
+++ week2/homework-商家版.py
@@ -5,18 +5,20 @@
 import shelve,os
 biz_dbfile='goods.txt'
 
-
 '''导入老的商品'''
 if os.path.isfile(biz_dbfile+'.dat'):
     dbwrite = shelve.open(biz_dbfile)
     goodslist_old=dbwrite.get('goodslist')
     print(goodslist_old)
+    if goodslist_old is None:
+        print('数据文件已存在，但列表为空')
+        goodslist_old={}
 else:
     print('第一次运行，将创建数据文件')
     dbwrite = shelve.open(biz_dbfile)
     goodslist_old={}
 
-choice=input('''商家版，请选择：
+choice=input('''请选择：
 1、修改或新增商品
 2、删除商品''')
 
@@ -32,7 +34,6 @@ if choice.isdigit() and int(choice)==1:
             cost=int(cost)
             goodslist_new[goods]=cost
 
-
         else:
             print('商品价格必需是数字，请重新输入')
             continue
@@ -40,6 +41,7 @@ if choice.isdigit() and int(choice)==1:
         #继续添加商品选择
         addchoice=input('是否继续添加商品y/n：')
         if addchoice == 'y' or addchoice=='':
+            print('已经选择添加的商品有%s'%goodslist_new)
             continue
         else:
             print(goodslist_new)
@@ -53,13 +55,31 @@ if choice.isdigit() and int(choice)==1:
             exit()
 elif choice.isdigit() and int(choice)==2:
     '''下线商品'''
+    dellist=[]
     while True:
         goods=input('请输入商品名称：').strip()
-        try:
-            goodslist_old[goods]
+        print(goodslist_old.get(goods))
+        if  goodslist_old.get(goods):
             goodslist_old.pop(goods)
-            print(goods)
-        except:
+            dellist.append(goods)
+            print('本次删除的产品为%s'%goods)
+            delchoice=input('是否继续删除其它商品y/n，默认是y')
+            if delchoice == 'y' or delchoice == '':
+                print('当前产品还有%s'%goodslist_old)
+                print('待删除的产品为%s'%dellist)
+                continue
+            else:
+                print('待删除的产品为%s' % dellist)
+                affirm=input('是否确认删除y/n，默认是y')
+                if affirm == 'y' or affirm == '':
+                    print(goodslist_old)
+                    dbwrite['goodslist']=goodslist_old
+                    print('商品已经被下架')
+                    exit()
+                else:
+                    print('本次将不会下架任何商品')
+                    exit()
+        else:
             print('您输入的商品名称不存在，请重新输入')
 else:
     print('输入不正确，请重新输入')
