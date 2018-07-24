ce6f5b9f312b764a3a2cd9091dfc174f240ce99a
孙杰彬 <sunjb@my089.net>
2018-07-24 20:54:56 +0800

    购物车作业

--- week2/homework购物车.py
+++ week2/homework购物车.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
 # Python version: python3
 # Auther: sunjb
-import sys
+import sys,shelve,os
 
-list=[('iphone',5800),('mac',12000),('bike',800),('starbuck',38),('p10',5200)]
+#list=[('iphone',5800),('mac',12000),('bike',800),('starbuck',38),('p10',5200)]
+biz_dbfile='goods.txt'
+user_dbfile='user.txt'
+dbread=shelve.open(biz_dbfile)
+userwrite=shelve.open(user_dbfile)
+'''判断商家数据'''
+if os.path.isfile(biz_dbfile+'.dat'):
+    dbwrite = shelve.open(biz_dbfile)
+    goodslist=dbwrite.get('goodslist')
+    list=list(goodslist.items())
+    #print(list)
+
+    if goodslist is None:
+        print('还未上线任何商品')
+        goodslist={}
+else:
+    print('本店还未开业')
+    exit(1)
+
+'''判断用户数据'''
+salary=userwrite.get('userprice')
+if salary is None:
+    print('欢迎首次光临本店')
+    salary = input('请告诉我你有多少钱')
+    userlist=[]
+else:
+    userlist=userwrite.get('userlist')
+    print(userlist,salary)
+    salary=str(salary)
+
 yourlist=[]
-salary=input('请告诉我你有多少钱')
+
 if salary.isdigit():
    salary=int(salary)
    while True:
        print('以下是可购买的商品列表'.center(50,'#'))
        for i,j in enumerate(list):
            print(i,j)
-       choice=input('请输入你要购买的商品编号')
+       choice=input('请输入你要购买的商品编号，结束请按q')
        if choice.isdigit():
            choice=int(choice)
            if choice>=0 and choice<len(list):
@@ -33,46 +62,19 @@ if salary.isdigit():
        elif choice is 'q':
            print('你购买的商品为%s' % yourlist)
            print('你的钱还剩余\033[31;1m%s\033[0m' % salary)
-           exit(0)
+           confirm=input('是否确认购买？y/n')
+           if confirm == 'y' or confirm == '':
+               userlist.extend(yourlist)
+               userprice=salary
+               print(userprice,userlist)
+               print(userlist,userprice)
+               userwrite['userlist']=userlist
+               userwrite['userprice']=userprice
+               print('商品信息及余额已保存')
+               exit(0)
+           else:
+               print('本次购买不会生效')
        else:
            print('你输入的不正确，请重新输入')
 else:
     print('请输入数字')
-
-'''
-try:
-    yourmoney=int(input('请告诉我你有多少钱'))
-    print(type(yourmoney))
-except:
-    print('请输入整数数字')
-
-
-list={
-    1:['iphone',5800],
-    2:['mac',12000],
-    3:['starbuck',38],
-    4:['bike',800]
-}
-
-yourgoods=[]
-
-while True:
-        print(list)
-        choice=input('你要买买买吗y/n')
-        if choice == 'n':
-            print('你购买的商品为%s,余额为%s'%(yourgoods,yourmoney))
-            break
-        else:
-            num = int(input('请选择你要的商品编号'))
-            goods = list.get(num)[0]
-            price = list.get(num)[1]
-            yourmoney = yourmoney - price
-            if yourmoney < 0:
-                print('你都没钱了还买个屁,赶紧结账滚')
-                break
-            else:
-                print('您当前余额还有:%s'%(yourmoney))
-                yourgoods.append(goods)
-                print('您当前已选商品为%s'%(yourgoods))
-                continue
-'''
