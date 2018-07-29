eaa94c1ede76dceac5875971cb3339c5cecd5653
sunjiebin <sunjb@my089.net>
2018-07-29 10:34:05 +0800

    类的继承

--- week6/08类的继承4.py
+++ week6/08类的继承4.py
@@ -14,7 +14,7 @@ class people(object):
 class relation(object):
     def make_friends(self, obj):
         print('%s making friends with %s' % (self.name, obj.name))
-        self.friends.append(obj.name)
+        self.friends.append(obj)
 class man(people,relation):
     def sleep(self,obj):
         print('%s is sleeping with %s'%(self.name,obj.name))
@@ -24,8 +24,10 @@ class woman(people,relation):
 m1=man('sun', 18)
 w1=woman('mi', 16)
 m1.talk()
-m1.make_friends(w1)
 w1.name='helo'
+m1.make_friends(w1)
+
+print(m1.friends[0].name)
 '''这里一直不明白，当
 self.friends.append(obj)  print(m1.friends[0].name)时，
 w1.name不管怎么设置，不管放在哪，执行结果都是helo。
@@ -38,8 +40,70 @@ m1.make_friends(w1)
 w1.name='helo'
 这样就打印mi
 '''
+'''
+终于弄明白了，当
+self.friends.append(obj)  print(m1.friends[0].name)时，
+self.friends.append(obj) 追加的是一个内存对象，是w1的内存地址，而不是一个固定的字符串，
+所以当内存地址里面的变量变化时，那么这个self.friends也会跟着变。
+m1.friends[0].name是从w1内存里面读出来的name.
+1、当顺序为如下时
+w1.name='helo'
+m1.make_friends(w1)
+print(m1.friends[0].name)
+w1.name='helo'已经将w1的内存地址修改。所以此时
+m1.make_friends(w1)时，下面函数中obj.name为w1.name，打印helo,
+self.friends.append(obj)这里面传的w1，而此时w1.name已经是helo，所以也为helo
+    def make_friends(self, obj):
+        print('%s making friends with %s' % (self.name, obj.name))
+        self.friends.append(obj)
+此时obj.name=helo, m1.friends[0].name=helo
+2、当顺序如下时
+m1.make_friends(w1)
+w1.name='helo'
+print(m1.friends[0].name)
+由于先执行了make_friends(w1),所以make_friends函数里面的obj.name此时是mi，
+此时self.friends.append(obj)此时加载了w1的内存地址。
+而后执行了w1.name='helo'，这时候将w1内存里面的name指向改为了helo
+所以，对应的self.friends变量里面的self.friends[0].name也跟着变了。
+self.friends[0].name == w1.name
+这时候打印出来的m1.friends[0].name也就变成了helo
+此时obj.name=mi,m1.friend[0].name=helo
+
+当
+self.friends.append(obj.name)  print(m1.friends[0])时，
+self.friends.append(obj.name) 追加的是一个实例化的地址obj.name，即w1.name,这是一个固定的字符串
+m1.friends[0]就是读取的w1.name
+情景1：
+w1.name='helo'
+m1.make_friends(w1)
+    def make_friends(self, obj):
+    #obj.name=w1.name，所以打印出helo
+        print('%s making friends with %s' % (self.name, obj.name))
+     #这里面也是同理，传入的obj.name其实就是helo这个字符串，而不是一个变量   
+        self.friends.append(obj.name)
+#这里打印m1.friends[0]其实就是打印了obj.name，即helo
 print(m1.friends[0])
-#m1.sleep(w1)
+此时obj.name=helo，m1.friends[0]=helo
+
+情景2：
+先执行m1.make_friends(w1)，再执行w1.name='helo'
+m1.make_friends(w1)
+    def make_friends(self, obj):
+        #这里由于w1.name还未执行，所以obj.name还是原来的mi
+        print('%s making friends with %s' % (self.name, obj.name))
+        #同样的，这里的obj.name==原来的w1.name，也是mi
+        所以此时的friends.append[0]的值已经是一个实例化的字符串mi
+        self.friends.append(obj.name)
+#此时再执行w1.name=helo，只是修改了people里面的self.name=helo，与self.friends[]没有任何关系了。
+ w1.name='helo'   
+#所以，此时的self.friends[0]打印出来的是mi
+print(m1.friends[0])
+此时obj.name=mi,m1.friends[0]=mi
+
+'''
+
+
+
 
 '''上面的类中，man和relation都没有定义name参数，但是它们
 却都能够读到self.name，这是因为在执行在执行sun=man('sun',18)时，
--- week6/10学校老师学员类的组合使用.py
+++ week6/10学校老师学员类的组合使用.py
@@ -13,7 +13,7 @@ class School(object):
         print('为学员%s办理入学手续'%stu_obj.name)
         self.students.append(stu_obj)
     def hire(self,staff_obj):
-        print('为老师%s办理入职手续'%staff_obj.name)
+        print('%s机构为老师%s办理入职手续'%(self.name,staff_obj.name))
         self.staffs.append(staff_obj)
 class SchoolMember(object):
     def __init__(self,name,age,sex):
@@ -49,25 +49,52 @@ class Student(SchoolMember):
     def pay_tutition(self,amount):
         print('%s has paid tutition for $[%s]'%(self.name,amount))
 
+'''实例化School,将self.name,self.addr赋值'''
 school=School('老男孩','北京')
 
+'''实例化Teacher，添加两个老师'''
 t1=Teacher('oldboy',56,'M',200000,'linux')
 t2=Teacher('alex',22,'M',300000,'python')
-
+'''实例化Student，添加两个学生'''
 s1=Student('sun',28,'M',1001,'python')
 s2=Student('mi',25,'F',1002,'linux')
-
+'''调用实例化后的方法'''
 t1.tell()
+t1.teach()
 s1.tell()
+print('打印school.name',school.name)
 
+'''在实例化后School中，引入t1
+过程解析：
+school.hire(t1)   通过school，将t1的内存地址放在hire函数的参数里面。
+执行了以下方法，
+这个方法里面的self便是school，所以self.name也就是school.name
+staff_obj就是传进来的t1，所以staff_obj.name其实就是t1.name
+   def hire(self,staff_obj):   
+        print('%s机构为老师%s办理入职手续'%(self.name,staff_obj.name))
+下面这行的self.staffs是School类里面定义的构造函数，相当于school.staffs.append
+        self.staffs.append(staff_obj)
+'''
 school.hire(t1)
 school.enroll(s1)
 school.enroll(s2)
 
 #print(school.students[0].grade)
+'''打印school.students,这是一个列表，列表里面的元素是s1,s2的内存地址。
+school.students[0]是s1的内存地址，school.students[2]是s2的内存地址。
+'''
 print(school.students)
 print(school.staffs)
+
+'''school.staffs[0]即我们前面添加的t1的内存地址，所以
+school.staffs[0].teach() == t1.teach()
+'''
 school.staffs[0].teach()
+#和上面的效果等效
+t1.teach()
 
 for stu in school.students:
+    stu.pay_tutition(8000)
+'''等效于'''
+for stu in [s1,s2]:
     stu.pay_tutition(8000)
