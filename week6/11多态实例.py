e2a8be0d27e1fdb6e095d08294c14cfc54a30509
sunjiebin <sunjb@my089.net>
2018-08-05 22:31:06 +0800

    静态方法、属性方法

--- week6/11多态实例.py
+++ week6/11多态实例.py
@@ -7,13 +7,19 @@
 它们的目的都是为了代码的重用。而多太是为实现另一个目的
 接口的重用。
 '''
-
+'''
+通过@staticmethod静态化之后，实际上下面的def就变成了一个函数，
+和类里面其它方法不一样了，所以self也并不需要了。这个叫静态方法
+'''
+'''静态方法只是名义上归类管理，实际在静态方法里面访问不到类或实例中的任何属性'''
 class Animal(object):
     def __init__(self,name):
         self.name=name
     @staticmethod
     def animal_talk(obj):
         obj.talk()
+        #已经不能访问self.name
+        #print(self.name)
 class dog(Animal):
     def talk(self):
         print('%s wangwang!'%self.name)
