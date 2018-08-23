8904d0e0e53f7935ba695586ab67b93e04011bea
孙杰彬 <sunjb@my089.net>
2018-08-23 20:49:20 +0800

    selectors实现并发

---
+++ week10/17selectors并发socket.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Python version: python3
+# Auther: sunjb
+import selectors,socket
+
+server=socket.socket()
+server.bind(('localhost', 6900))
+server.listen()
+server.setblocking(False)
+sel=selectors.DefaultSelector()
+
+def accept(server, mask):
+    conn,addr=server.accept()
+    print(conn,addr,mask)
+    conn.setblocking(False)
+    sel.register(conn,selectors.EVENT_READ,read)
+
+def read(conn,mask):
+    try:
+        data=conn.recv(1024)
+        if data:
+            print('收到数据',repr(data),conn)
+            conn.send(data)
+        else:
+            print('客户端断开连接',conn)
+            sel.unregister(conn)
+            conn.close()
+    except Exception:
+        print('客户端断开连接', conn)
+        sel.unregister(conn)
+        conn.close()
+
+sel.register(server, selectors.EVENT_READ, accept)
+
+
+while True:
+    events=sel.select()
+    print(events)
+    for key,mask in events:
+        callback=key.data
+        callback(key.fileobj,mask)
