55d04c82bb3cf4b4743d199944aa931a123d99a9
孙杰彬 <sunjb@my089.net>
2018-08-24 19:50:44 +0800

    select

--- week10/17高并发连接client.py
+++ week10/17高并发连接client.py
@@ -13,7 +13,7 @@ messages = [ b'This is the message. ',
 server_address = ('localhost', 6900)
 
 # Create a TCP/IP socket
-socks = [ socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(10)]
+socks = [ socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(500)]
 
 # Connect the socket to the port where the server is listening
 print('connecting to %s port %s' % server_address)
@@ -24,12 +24,12 @@ for message in messages:
 
     # Send messages on both sockets
     for s in socks:
-        print('%s: sending "%s"' % (s.getsockname(), message) )
+        print('%s: sending "%s"' % (s.getsockname(), message))
         s.send(message)
 
     # Read responses on both sockets
     for s in socks:
         data = s.recv(1024)
-        print( '%s: received "%s"' % (s.getsockname(), data) )
+        print( '%s: received "%s"' % (s.getsockname(), data))
         if not data:
-            print(sys.stderr, 'closing socket', s.getsockname() )
+            print(sys.stderr, 'closing socket', s.getsockname())
