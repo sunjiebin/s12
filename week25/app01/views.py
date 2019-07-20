e759febe41d2d793187e865046d41ab88a38ccc0
sun <445335413@qq.com>
2019-07-20 13:29:49 +0800

    利用模板函数实现标签筛选功能

--- week25/app01/views.py
+++ week25/app01/views.py
@@ -8,6 +8,7 @@ def article(request,**kwargs):
     '''当v为0时，相当于数据库里面id=0，实际上时不存在的，我们定义为代表查询全部数据，所以当v=0，我们就不设置该查询条件。在filter查询时
     就会把所有匹配的数据查出来'''
     for k,v in kwargs.items():
+        #注意从urls.py里面拿过来的kwargs字典里面的值是字符串，即v是字符串不是数字。而从数据库里面拿到的是数字。category.id是数字。
         #默认v是字符串，我们将他转化为数字，这样在html模板中就能够和category取到的id相比较了。否则一个字符串一个数字没法相等
         kwargs[k]=int(v)
         if v != '0':
@@ -16,4 +17,4 @@ def article(request,**kwargs):
     category=models.Category.objects.all()
     articletype=models.ArticleType.objects.all()
     result=models.Article.objects.filter(**dic)
-    return render(request,'article.html',{'result':result,'category':category,'articletype':articletype,'kwargs':kwargs})
+    return render(request,'article_template.html',{'result':result,'category':category,'articletype':articletype,'kwargs':kwargs})
